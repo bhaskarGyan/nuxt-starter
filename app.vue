19feda9e3d87675cbbc52c166ed7e83f4d91aa85
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-09-19 21:45:45 +0530

    Add tailwing

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <NuxtWelcome />
+    <div class="flex text-red-900 p-10">Nuxt3 with InlineCss False</div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-   
+    modules: ['@nuxtjs/tailwindcss'],
     router: {
         base: '/nuxt3-brotli/'
+      },
+      experimental:{
+        inlineSSRStyles:false
       }
   });
