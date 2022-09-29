1b4e6b04c141eae9193b502dcfbb4286de4fa3a5
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-09-29 23:57:30 +0530

    Update seo tag

--- app.vue
+++ app.vue
@@ -3,12 +3,10 @@
     <Html lang="en">
       <Head>
         <Title>Nuxt Upcoming feature Playground Repo</Title>
-        <Meta name="google-site-verification" content="1cvSCDTUkWtJFDC2X3tWaciOGNmjhAnBSWQJbr0FPV0" />
+        <Meta name="google-site-verification" content="BB9WpyZLCV9k9nGqjv_KLkrdf8R_UnnPOjTK45M_CW0" />
 
       </Head>
     </Html>
     <NuxtWelcome />
   </div>
 </template>
-<script setup>
-</script>
