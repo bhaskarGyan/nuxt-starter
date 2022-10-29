b95fc7a37165a5b36b6b745a6d3eee0e058a9d31
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-29 12:28:03 +0530

    Add nuxt-cache-ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,42 @@ export default defineNuxtConfig({
     },
     router: {
         base: '/nuxt3-brotli/'
-      }
+      },
+      modules: [
+        ['nuxt-cache-ssr', {
+          // Can be disable per enviroment, like in dev
+          enabled: true,
+          store: {
+            // Plceholder for store type, will be usable after Redis Release
+            type: 'memory',
+            // maximum number of pages to store in memory
+            // if limit is reached, least recently used page
+            // is removed.
+            max: 500,
+            // number of Millisecond to store this page in cache
+            ttl: 1000 * 60 * 180 // 3 hours
+          },
+          pages: [
+            // these are prefixes of pages that need to be cached
+            '/',
+    
+          ],
+          key: (route: string, headers: any, device: Device) => {
+    
+            // Link to the function will be broken, so cannot use any imported modules or custom functions
+            //sample of using device to generate key
+    
+            const { userAgent, ...deviceType } = device
+            const key = [route];
+            Object.keys(deviceType).forEach(val => {
+              if (deviceType[val]) {
+                key.push(val)
+              }
+            })
+            // returned value will be hashed using ohash
+            return key.join("-")
+          }
+        }
+        ],
+      ],
   });
